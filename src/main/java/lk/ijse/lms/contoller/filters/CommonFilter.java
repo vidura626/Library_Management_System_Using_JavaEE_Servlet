748355e6fea278d993faaea3e90f026dfd1c5196
vidura626 <wijesinghemap@gmail.com>
2023-05-10 12:04:11 +0530

    CommonFiler.java filter added.

--- src/main/java/lk/ijse/lms/contoller/filters/CommonFilter.java
+++ src/main/java/lk/ijse/lms/contoller/filters/CommonFilter.java
@@ -1,23 +1,28 @@
 package lk.ijse.lms.contoller.filters;
 
+import lk.ijse.lms.contoller.entity.Book;
+import lk.ijse.lms.contoller.entity.Catogery;
+import lk.ijse.lms.util.FactoryConfiguration;
+import org.hibernate.Session;
+
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
-@WebFilter
-public class CommonFiler implements Filter {
+//@WebFilter(urlPatterns = "/")
+public class CommonFilter implements Filter {
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        Filter.super.init(filterConfig);
+        System.out.println("Started");
     }
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("Filter proccess");
         filterChain.doFilter(servletRequest,servletResponse);
     }
 
     @Override
     public void destroy() {
-        Filter.super.destroy();
     }
 }
